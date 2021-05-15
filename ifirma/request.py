a82ad85fad4eb1f9da265d45e31aa5e4f9386656
Paweł Nowosielski <pawel.nowosielski@golem.network>
2021-05-15 10:13:46 +0200

    refactor: simplifies url paths

--- ifirma/request.py
+++ ifirma/request.py
@@ -7,13 +7,11 @@ import requests
 from ifirma.config import API_USER, API_KEY
 from ifirma.serializer import make_email, make_invoice
 
-API_URL='https://www.ifirma.pl/iapi'
-API_CREATE_INVOICE_URL = "{}/fakturakraj.json".format(API_URL)
-API_SEND_EMAIL_URL_WITH_PARAM = "{}/fakturakraj/send/".format(API_URL), "{}.json?wyslijEfaktura=true"
-API_GET_INVOICES_LIST_URL="{}/faktury.json".format(API_URL)
-API_GET_INVOICE_DETAILS_URL_WITH_PARAM="{}/fakturakraj/".format(API_URL), "{}.json"
-API_DOWNLOAD_INVOICE_URL_WITH_PARAM="{}/fakturakraj/".format(API_URL), "{}.pdf.{}"
 INVOICE_KEY_NAME = "faktura"
+API_URL = 'https://www.ifirma.pl/iapi'
+API_INVOICE_OPER = API_URL + '/fakturakraj'
+API_INVOICE_LIST = API_URL + '/faktury.json'
+
 
 def sign_raw(data, key):
     api_key = key if isinstance(key, bytes) else unhexlify(key)
@@ -25,38 +23,38 @@ def sign(request_url, data, api_user=None, api_key=None, api_scope=None):
     api_user, api_key, api_scope = api_user or API_USER, api_key or API_KEY, api_scope or INVOICE_KEY_NAME
     base_url = request_url.split('?')[0]
 
-    sign_data = "{}{}{}{}".format(base_url, api_user, api_scope, data)
+    sign_data = f"{base_url}{api_user}{api_scope}{data}"
     api_token = sign_raw(sign_data, api_key)
 
     return "IAPIS user={}, hmac-sha1={}".format(api_user, api_token)
 
 def send_invoice(invoice):
+    url = f"{API_INVOICE_OPER}.json"
     json_req = json.dumps(make_invoice(invoice))
-    auth = sign(API_CREATE_INVOICE_URL, json_req)
+    auth = sign(url, json_req)
    
     headers = {'Content-Type': 'application/json; charset=utf8', 'Authentication': auth}
-    r = requests.post(API_CREATE_INVOICE_URL, data=json_req, headers=headers)
+    r = requests.post(url, data=json_req, headers=headers)
 
     r.raise_for_status()
     return InvoiceResponse(r.json())
 
 def send_email(invoice_id, email_address, text):
     json_req = json.dumps(make_email(email_address, text))
-    url, param = API_SEND_EMAIL_URL_WITH_PARAM
+    url = f"{API_INVOICE_OPER}/send/{invoice_id}.json?wyslijEfaktura=true"
     
-    request_url = url + param.format(invoice_id)
-    auth = sign(request_url, json_req)
+    auth = sign(url, json_req)
 
     headers = {'Content-Type': 'application/json; charset=utf8', 'Authentication': auth}
-    r = requests.post(request_url, data=json_req, headers=headers)
+    r = requests.post(url, data=json_req, headers=headers)
 
     r.raise_for_status()
     return r.json()
 
 def get_invoices_list(date_from, date_to, invoice_type='prz_faktura_kraj', page=1, items_on_page=20):
-    url = API_GET_INVOICES_LIST_URL
-
-    request_url = url + "?typ={}&dataOd={}&dataDo={}&strona={}&iloscNaStronie={}".format(invoice_type, date_from, date_to, page, items_on_page)
+    request_url = API_INVOICE_LIST + "?typ={}&dataOd={}&dataDo={}&strona={}&iloscNaStronie={}".format(
+        invoice_type, date_from, date_to, page, items_on_page
+    )
     auth = sign(request_url, '')
 
     headers = {'Content-Type': 'application/json; charset=utf8', 'Authentication': auth}
@@ -66,25 +64,21 @@ def get_invoices_list(date_from, date_to, invoice_type='prz_faktura_kraj', page=
     return r.json()
 
 def get_invoice(invoice_id):
-    url, param = API_GET_INVOICE_DETAILS_URL_WITH_PARAM
-
-    request_url = url + param.format(invoice_id)
-    auth = sign(request_url, '')
+    url = f"{API_INVOICE_OPER}/{invoice_id}.json"
+    auth = sign(url, '')
 
     headers = {'Content-Type': 'application/json; charset=utf8', 'Authentication': auth}
-    r = requests.get(request_url, headers=headers)
+    r = requests.get(url, headers=headers)
 
     r.raise_for_status()
     return r.json()
 
 def download_invoice(invoice_id, path, type="single"):
-    url, param = API_DOWNLOAD_INVOICE_URL_WITH_PARAM
-
-    request_url = url + param.format(invoice_id, type)
-    auth = sign(request_url, '')
+    url = f"{API_INVOICE_OPER}/{invoice_id}.pdf.{type}"
+    auth = sign(url, '')
     
     headers = {'Accept': 'application/pdf', 'Authentication': auth}
-    r = requests.get(request_url, headers=headers)
+    r = requests.get(url, headers=headers)
 
     r.raise_for_status()
     path.write_bytes(r.content)
--- sample_invoice/main.py
+++ sample_invoice/main.py
@@ -1,6 +1,16 @@
-import sys
+import os, sys
+from pathlib import Path
+
+temp_dir = Path("/tmp")
+script_path = os.path.realpath(__file__)
+module_path = os.path.dirname(os.path.dirname(script_path))
+sys.path.append(module_path)
+print("Module path: " + module_path)
+
 from ifirma.yaml_parser import parse
-from ifirma.request import send_invoice, send_email
+from ifirma.request import send_invoice, send_email, download_invoice
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 1 or not sys.argv[1].strip():
@@ -21,3 +31,7 @@ if __name__ == "__main__":
     if create_invoice_response.success and (email_address := task.get('send_to')):
         email_send_response = send_email(invoice_id, email_address, task['message'])
         print(f'Email sent {email_send_response}')
+
+    download_path = temp_dir / f"invoice_{invoice_id}.pdf"
+    download_invoice(invoice_id, download_path)
+    print(f"Invoice copied to {download_path}")
