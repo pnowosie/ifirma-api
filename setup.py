8b6f99dd763e8e74463f4656582ee1eb71f4872d
Paweł Nowosielski <pawel.nowosielski@golem.network>
2022-10-14 16:29:07 +0200

    release new package version to pypi (why manually)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pnowosie.ifirma-api",  # Replace with your own username
-    version="1.2.0",
+    version="1.3.0",
     author="Paweł Nowosielski",
     author_email="nowosielski@gmail.com",
     description="IFirma API client",
